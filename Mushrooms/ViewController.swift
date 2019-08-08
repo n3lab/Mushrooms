30948d777722a84e51bf9493f966f24ba75ee4ff
n3lab <n3laboratory@yandex.ru>
2019-08-08 03:00:39 +0300

    Initial Commit

---
+++ Mushrooms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mushrooms
+//
+//  Created by n3deep on 08/08/2019.
+//  Copyright © 2019 n3deep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
